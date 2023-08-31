bc658bbbdd9d42e06b614cf5963fa9eadb94c390
iiweby <danielparedesmilian@gmail.com>
2023-08-31 11:28:58 -0500

    Starting Java

---
+++ src/Welcome.java
@@ -0,0 +1,9 @@
+// First App
+
+public class Welcome {
+    public static void main(String[] args) throws Exception {
+        int n = 7;
+        int m = 5;
+        System.out.println(m + n);
+    }
+}
